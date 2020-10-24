9e85c1a5770c0cfb769c56786cd7acca9586eff6
Jammie623 <71751181+Jammie623@users.noreply.github.com>
2020-10-24 08:42:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,13 @@ basic.forever(function () {
             led.plot(4 - J, 4 - (I - J))
         }
         basic.pause(100)
-        basic.clearScreen()
-        for (let I = 0; I <= 8; I++) {
-            for (let J = 0; J <= 4; J++) {
-                led.plot(J, I - J)
-                basic.pause(100)
-            }
+    }
+    basic.clearScreen()
+    for (let I = 0; I <= 8; I++) {
+        for (let J = 0; J <= 4; J++) {
+            led.plot(J, I - J)
         }
+        basic.pause(100)
     }
     basic.clearScreen()
 })
